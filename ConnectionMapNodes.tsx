f3b5f4d942b03d4c4aba7b56c2e5b3771cf041ce
TheTokolosh <tristan.edwards.home@gmail.com>
2025-05-22 12:19:20 +1000

    Add files via upload

---
+++ ConnectionMapNodes.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="ConnectionMapNodes" tilewidth="20" tileheight="20" tilecount="256" columns="16">
+ <image source="../images/ConnectionMapNodes.png" width="320" height="320"/>
+</tileset>
---
+++ MapTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="MapTileset" tilewidth="20" tileheight="40" tilecount="5120" columns="64">
+ <image source="../images/MapTileset.png" width="1280" height="3200"/>
+</tileset>
